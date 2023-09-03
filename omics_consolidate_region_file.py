97a1351f3cec59494f256f4c41d930fe8cc170f8
Ileen <jhaney@example.com>
2023-09-03 13:24:27 -0400

    initial commit

--- omics_consolidate_region_file.py
+++ omics_consolidate_region_file.py
@@ -4,25 +4,26 @@ from datetime import date
 
 def get_omics_regions():
     """
-    Returns a list of AWS regions that offer the omics (HealthOmics) service.
+    Returns a list of AWS regions that offer the omics (HealthOmics) service
     """
     s = Session()
     omics_regions = s.get_available_regions("omics")
     return omics_regions
 
+
 def get_stores(client, store_type):
     """
     Returns a list of stores of the specified type for the given client.
     """
-    if store_type == 'sequence':
+    if store_type == "sequence":
         response = client.list_sequence_stores()
-        key = 'sequenceStores'
-    elif store_type == 'annotation':
+        key = "sequenceStores"
+    elif store_type == "annotation":
         response = client.list_annotation_stores()
-        key = 'annotationStores'
-    elif store_type == 'variant':
+        key = "annotationStores"
+    elif store_type == "variant":
         response = client.list_variant_stores()
-        key = 'variantStores'
+        key = "variantStores"
     else:
         raise ValueError(f"Invalid store type: {store_type}")
 
@@ -31,31 +32,32 @@ def get_stores(client, store_type):
         stores = response[key]
         # Add the store type to each store
         for store in stores:
-            store['type'] = store_type
+            store["type"] = store_type
         return stores
     else:
         return []
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     omics_regions = get_omics_regions()
-    
+
     # Get the current date and format it as YYYY-MM-DD
     today = date.today()
-    date_str = today.strftime('%Y-%m-%d')
-    
+    date_str = today.strftime("%Y-%m-%d")
+
     # Create the output file name
-    output_file_name = f'omics_data_stores_{date_str}.txt'
-    
+    output_file_name = f"omics_data_stores_{date_str}.txt"
+
     # Open the output file for writing
-    with open(output_file_name, 'w') as f:
+    with open(output_file_name, "w") as f:
         for region in omics_regions:
             # Create a client for AWS HealthOmics
-            client = boto3.client('omics', region_name=region)
+            client = boto3.client("omics", region_name=region)
 
             # Get the stores for each type
-            sequence_stores = get_stores(client, 'sequence')
-            annotation_stores = get_stores(client, 'annotation')
-            variant_stores = get_stores(client, 'variant')
+            sequence_stores = get_stores(client, "sequence")
+            annotation_stores = get_stores(client, "annotation")
+            variant_stores = get_stores(client, "variant")
 
             # Combine all stores into a single list
             all_stores = sequence_stores + annotation_stores + variant_stores
@@ -63,5 +65,5 @@ if __name__ == '__main__':
             # Print the details for each store
             for store in all_stores:
                 line = f"Type: {store['type']}, Date: {today}, Name: {store['name']}, Region: {region}, Store ID: {store['id']}\n"
-                print(line, end='')
+                print(line, end="")
                 f.write(line)
