21c82aaed716b45a72b4aa9a75e6bc15ec2c7c43
CICD-Tester <biocloud2022@gmail.com>
2024-08-07 07:55:16 -0400

    add docstring to script

--- omics_consolidate_region_file.py
+++ omics_consolidate_region_file.py
@@ -1,20 +1,19 @@
+"""
+This script retrieves AWS HealthOmics data store details from 
+multiple regions and consolidates them into a CSV file.
+"""
+
 from datetime import date
 import boto3
 import pandas as pd
-import botocore.exceptions  # Import botocore exceptions here
+import botocore.exceptions  
 
 def get_omics_regions():
-    """
-    Returns a list of AWS regions offering the omics (HealthOmics) service
-    """
     session = boto3.session.Session()
     omics_regions = session.get_available_regions("omics")
     return omics_regions
 
 def get_stores(client, store_type):
-    """
-    Returns a list of stores of the specified type for the given client, with improved error handling.
-    """
     try:
         if store_type == "sequence":
             response = client.list_sequence_stores()
@@ -28,7 +27,6 @@ def get_stores(client, store_type):
         else:
             raise ValueError(f"Invalid store type: {store_type}")
 
-        # Check and print the response
         print(f"Response for {store_type} stores in {client.meta.region_name}: {response}")
 
         if key in response:
@@ -37,14 +35,14 @@ def get_stores(client, store_type):
                 store["type"] = store_type
             return stores
         else:
-            return []  # Return an empty list if the key is not found
+            return []  
     except botocore.exceptions.ClientError as error:
         error_code = error.response['Error']['Code']
         if error_code == 'UnrecognizedClientException':
             print(f"ERROR: UnrecognizedClientException for {store_type} stores in {client.meta.region_name}. Check your AWS credentials and configuration.")
         else:
             print(f"ERROR: Unexpected error retrieving {store_type} stores in {client.meta.region_name}: {error}")
-        return []  # Return empty list to handle errors gracefully
+        return []  
 
 if __name__ == "__main__":
     omics_regions = get_omics_regions()
@@ -54,10 +52,8 @@ if __name__ == "__main__":
     final_df = None
 
     for region in omics_regions:
-        # Create a client for AWS HealthOmics
         client = boto3.client("omics", region_name=region)
 
-        # Get the stores for each type
         sequence_stores = get_stores(client, "sequence")
         annotation_stores = get_stores(client, "annotation")
         variant_stores = get_stores(client, "variant")
